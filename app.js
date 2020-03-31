4ba727c595b2dd897cb5a34196a8b6674262cbd0
Vladislav Skorinov <57506835+losbiw@users.noreply.github.com>
2020-03-31 19:28:27 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3000;
-require('dotenv/config');
+if (process.env.NODE_ENV !== 'production') { require('dotenv/config'); }
 
 app.set('view engine', 'ejs');
 
