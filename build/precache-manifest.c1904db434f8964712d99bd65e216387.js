a6fb634f27ce59103ae9bf3041b3a94d816d7b2b
losbiw <skorinov41@gmail.com>
2020-10-31 03:31:01 +0200

    Add optimized build for heroku

---
+++ build/precache-manifest.c1904db434f8964712d99bd65e216387.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d58f7b0016df8d45713512c87ecee54f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dea23a681933f1f5971a",
+    "url": "./static/css/main.9696cdd3.chunk.css"
+  },
+  {
+    "revision": "eb5855bc482acf2565a7",
+    "url": "./static/js/2.0ee75def.chunk.js"
+  },
+  {
+    "revision": "2c598234c7a24970b5fdf032f602e7bc",
+    "url": "./static/js/2.0ee75def.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dea23a681933f1f5971a",
+    "url": "./static/js/main.c4ffd582.chunk.js"
+  },
+  {
+    "revision": "d90c5b93796701d5570a",
+    "url": "./static/js/runtime-main.8b0ae790.js"
+  },
+  {
+    "revision": "91f3cbe7b90caa6dc8297225c05abeb3",
+    "url": "./static/media/background.91f3cbe7.jpg"
+  }
+]);
